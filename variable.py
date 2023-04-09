b7b4875429544d926b2101c1e178413ecef637cb
KingTiger <KingTiger@gmail.com>
2023-04-10 02:55:36 +0800

    update

---
+++ variable.py
@@ -0,0 +1,32 @@
+# Python Variable Names
+counter = 100
+_count  = 100
+name1 = "King"
+name2 = "Tiger"
+Age  = 20
+zara_salary = 100000
+
+print (counter)
+print (_count)
+print (name1)
+print (name2)
+print (Age)
+print (zara_salary)
+
+# Delete a Variable
+counter = 100
+print (counter)
+
+del counter
+print (counter)
+
+# del var
+# del var_a, var_b
+
+# Multiple Assignment
+a = b = c = 100
+
+print (a)
+print (b)
+print (c)
+
