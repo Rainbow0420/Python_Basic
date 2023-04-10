e12d2512836ace5f79d2829cd4747b36e9f322de
KingTiger <KingTiger@gmail.com>
2023-04-10 10:17:29 +0800

    comit

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import os
+import cgi, cgitb 
 # This is a single line comment in python
 print("Hello World!")
 os.getcwd()
+form = cgi.FieldStorage() 
+
+# Get data from fields
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello World - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ('</body>')
+print ('</html>')
